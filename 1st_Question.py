1e7cc7dea2d2e57a69ed29209d7d08d73ca38786
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2022-02-24 18:29:58 +0700

    [ADD] simple-drawing-window1

--- 1st_Question.py
+++ 1st_Question.py
@@ -3,11 +3,11 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window1(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.human = QPixmap("images\human.png")
 
     def paintEvent(self, e):
         p = QPainter()
@@ -20,21 +20,21 @@ class Simple_drawing_window(QWidget):
             QPoint(130, 100), QPoint(100, 150),
         ])
 
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
+        p.setPen(QColor(255, 0, 0))
+        p.setBrush(QColor(255, 0, 0))
         p.drawPie(50, 150, 100, 100, 0, 180 * 16)
         
         p.drawPolygon(
-            [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400),]
+            [QPoint( 50, 200), QPoint(150, 200), QPoint(150, 400), QPoint(50, 400)]
             )
 
-        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.drawPixmap(QRect(200, 100, 320, 320), self.human)
         p.end()
 
 def main():
     app = QApplication(sys.argv)
 
-    w = Simple_drawing_window()
+    w = Simple_drawing_window1()
     w.show()
 
     return app.exec_()
