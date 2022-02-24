a01cf89c5a127e6475b443d0a24298b7d351e247
nvnnicky <99760765+nvnnicky@users.noreply.github.com>
2022-02-24 19:13:14 +0700

    Update 1st_Question.py

--- 1st_Question.py
+++ 1st_Question.py
@@ -30,6 +30,7 @@ class Simple_drawing_window1(QWidget):
 
         p.drawPixmap(QRect(200, 100, 320, 320), self.human)
         p.end()
+        
 class Simple_drawing_window2(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
@@ -53,11 +54,40 @@ class Simple_drawing_window2(QWidget):
 
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
+        
+ class Simple_drawing_window3(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QPixmap("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 0, 0))
+
+        p.setPen(QColor(255, 255, 0))
+        p.setBrush(QColor(255, 255, 0))
+    
+        
+        p.drawPolygon(
+            [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400), ]
+            )
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
 def main():
     app = QApplication(sys.argv)
 
     w = Simple_drawing_window1()
     w.show()
+    w2 = Simple_drawing_window2()
+    w2.show()
+    w3 = Simple_drawing_window3()
+    w3.show()
 
     return app.exec_()
 
